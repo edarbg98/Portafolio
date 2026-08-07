836c379db3b6575ccfc9e51415301e8d8b4ba15b
Edar - Devs <edar674@gmail.com>
2026-08-06 20:15:53 -0500

    Update autorizar.js

--- autorizar.js
+++ autorizar.js
@@ -2,23 +2,12 @@ export function obtenerUsuarioLogeado() {
     return localStorage.getItem("UsuarioLogeado");
 };
 
-export function buildSiteUrl(relativePath) {
-    const pathname = window.location.pathname.replace(/\/+$/, "");
-    const segments = pathname.split("/").filter(Boolean);
-    const webIndex = segments.indexOf("web");
-    const basePrefix = webIndex !== -1 && webIndex < segments.length - 1
-        ? `/${segments.slice(0, webIndex + 1).join("/")}/`
-        : "/";
-
-    return `${basePrefix}${relativePath.replace(/^\/+/, "")}`;
-}
-
 export function verificarAcceso() {
     const usuario = obtenerUsuarioLogeado()
 
     if (!usuario) {
         alert("Acceso Denegado, primero ingrese las credenciales");
-        window.location.href = buildSiteUrl("index.html");
+        window.location.href = "index.html";
         return false;
     }
 
